b7df50b4bd0402a71d83a065dc2ef8ff27e5d5a6
vojteq <vojteq17@gmail.com>
2020-05-22 19:12:01 +0200

    added some comments

--- tinder_bot.py
+++ tinder_bot.py
@@ -1,16 +1,12 @@
 import codecs
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import re
-import time
 import smtplib
 from email.mime.text import MIMEText
 import cv2
-import sys
-from io import BytesIO
 from time import sleep
 import dialogflow
 import os
@@ -23,7 +19,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = './diana-eoqlsq-98249f138627.json
 SESSION_ID = '118197476799899566966'
 
 
-class TinderBot():
+class TinderBot:
     keywords: []
     keywordsVerification: []
     keywordsVerificationKey: []
@@ -73,9 +69,11 @@ class TinderBot():
             except:
                 pass
 
+        # aby mozliwy byl powrot do pierwotnego okna bo logowanie jest w pop-upie
         baseWindow = self.driver.window_handles[0]
         self.driver.switch_to_window(self.driver.window_handles[1])
 
+        # logowanie kontem FB
         emailInput = self.driver.find_element_by_xpath(self.paths[3])
         emailInput.send_keys(email)
         passwordInput = self.driver.find_element_by_xpath(self.paths[4])
@@ -83,6 +81,7 @@ class TinderBot():
         confirmButton = self.driver.find_element_by_xpath(self.paths[5])
         confirmButton.click()
 
+        # powrot do pierwotnego okna
         self.driver.switch_to_window(baseWindow)
 
         # 6 -> localizationButton, 7 -> noNotificationsButton, 8 -> cookiesButton, 9 -> noLocalizationChangeButton
@@ -101,6 +100,7 @@ class TinderBot():
         r = self.findFace(i)
 
         try:
+            # jesli znajdzie odpowiednia ilosc zdjec, twarz na zdjeciu oraz opis nie zawiera slow zabronionych -> like
             if p and q and r:
                 swipeRightButton = wait.until(EC.element_to_be_clickable((By.XPATH, self.paths[10])))
                 swipeRightButton.click()
@@ -110,14 +110,14 @@ class TinderBot():
         except:
             pass
 
-        # zamkniecie ewentualnego powiadomienia o nowym matchu
+        # zamkniecie ewentualnego powiadomienia o nowym matchu (jak jest para to wyskakuje pop-up)
         try:
             continueSwiping = wait.until(EC.element_to_be_clickable((By.XPATH, self.paths[11])))
             continueSwiping.click()
         except:
             pass
 
-        # nie dodanie skrotu na pulpit
+        # nie dodanie skrotu na pulpit (czasem wyskakuje pop-up)
         try:
             dontAddShortcut = wait.until(EC.element_to_be_clickable((By.XPATH, self.paths[13])))
             dontAddShortcut.click()
@@ -125,14 +125,13 @@ class TinderBot():
             pass
 
     def chat_bot(self, xd, name_of_guy):
-
         session_client = dialogflow.SessionsClient()
         session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
         text_input = dialogflow.types.TextInput(text=xd, language_code=DIALOGFLOW_LANGUAGE_CODE)
         query_input = dialogflow.types.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session, query_input=query_input)
-        # sprawdzanie czy intent jest typu propozycja spotkania
 
+        # sprawdzanie czy intent jest typu propozycja spotkania
         if response.query_result.intent.display_name == "meeting prop":
             # jesli intent wiadomości to meeting prop - w Dialogflow folder z propozycjami spotkania
             # wysyłamy email
@@ -157,7 +156,6 @@ class TinderBot():
 
         server.sendmail(email, email, message.as_string())
 
-
     def chat(self):
         wait = WebDriverWait(self.driver, 5)
         try:
@@ -167,20 +165,18 @@ class TinderBot():
             print("cant open messages")
             return
         sleep(2)
-        # chat_windows = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'messageListItem')))
         chat_windows = self.driver.find_elements_by_class_name('messageListItem')
 
         counter = 0
         for convo in chat_windows:
-            if counter == 5:
+            # powrot po odpisaniu trzem osobom bo normalnie leci do konca listy chatow
+            if counter == 3:
                 return
             counter += 1
             convo.click()
             sleep(3)
-            # all_messages = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'msg')))
             all_messages = self.driver.find_elements_by_class_name('msg')
             last_message = all_messages[-1]
-            # last_message = all_messages   [0]
             print(str(last_message))
             # teraz sprawdzamy czy ostatnia wiadomość na czacie została napisana przez nas czy przez parę -
             # wiadomości napisane przez parę mają kolor #000
@@ -203,14 +199,17 @@ class TinderBot():
 
     # sprawdzenie czy opis zawiera slowa kluczowe ktorych nie chcemy
     # i potem sprawdzenie czy ewentulanie wystepuja w zlozeniach z innymi slowami, tak ze juz akceptujemy
+    # (typu jakies negacje na przyklad)
     def checkDescription(self):
         wait = WebDriverWait(self.driver, 5)
+        # flaga profileOk okresla czy akceptujemy profil, czyli czy nie znaleziono zadnych niechcianych rzeczy w opisie
         profileOk = True
         try:
-            # rozwiniecie opisu
+            # rozwiniecie opisu (klikniecie odpowiedniego przycisku)
             descriptionButton = wait.until(EC.element_to_be_clickable((By.XPATH, self.paths[18])))
             descriptionButton.click()
 
+            # pobranie opisu
             description = wait.until(EC.presence_of_element_located((By.XPATH, self.paths[19]))).text
 
             print("OPIS:\n", description)
@@ -222,13 +221,13 @@ class TinderBot():
                 if self.keywords[i] in description:
                     word = self.keywords[i].lower()
                     profileOk = False
-                    # jesli wystepuje, szukamy czy jest to fraza ktora nie jest zakazana
+                    # jesli wystepuje, szukamy czy jest to fraza ktora nie jest zakazana (np negacja)
                     for j in range(0, len(self.keywordsVerificationKey)):
                         if word == self.keywordsVerificationKey[j]:
                             for k in self.keywordsVerification[j] and not profileOk:
                                 if k in description:
                                     profileOk = True
-
+        # jak poleci jakis blad no to tez wyrzucamy profil
         except:
             profileOk = False
 
@@ -238,10 +237,12 @@ class TinderBot():
             exitDescription.click()
         except:
             pass
+        # zwracamy flage, a wiec czy profil jest odpowiedni
         return profileOk
 
-
     # proba znalezienia trzeciego zdjecia, jesli nie ma -> swipe left
+    # ilosc zdjec mozna zmieniac po prostu zmieniajac xpath bo sa przyciski do kazdego zdjecia
+    # (do zmiany powinna wystarczyc wartosc w ostatnim nawiasie kwadratowym xpathu)
     def checkPhotos(self):
         wait = WebDriverWait(self.driver, 3)
         try:
@@ -253,16 +254,20 @@ class TinderBot():
     def findFace(self, i):
         flag = False
 
+        # stworzenie katalogu dla zdjec (jesli nie istnieje (podana sciezka w loginInfo.py))
         if not os.path.exists(savePicturesDirectory):
             os.makedirs(savePicturesDirectory)
 
+        # zrobienie screenshota
         self.driver.get_screenshot_as_file('screenshot' + str(i) + '.png')
         img.append(cv2.imread('screenshot' + str(i) + '.png'))
         face_cascade = cv2.CascadeClassifier(faceLibraryPath)
 
+        # przyciecie zdjecia
         crop_img.append(img[i][100:650, 1000:1300])
-
+        # zapisanie przycietego
         cv2.imwrite("cropp" + str(i) + ".png", crop_img[i])
+        # szukanie twarzy
         img1.append(cv2.imread('cropp' + str(i) + '.png'))
         photoGray.append(cv2.cvtColor(img1[i], cv2.COLOR_BGR2GRAY))
         faces.append(face_cascade.detectMultiScale(
@@ -277,6 +282,7 @@ class TinderBot():
             print("no face detected")
             cv2.imwrite(savePicturesDirectory + str(i) + ".png", img1[i])
         else:
+            # jesli znaleziono twarz to szukamy emocji
             detector = FER()
             emotion, score = detector.top_emotion(img1[i])
             print(emotion, score)
@@ -298,7 +304,9 @@ photoGray = []
 crop_img = []
 img = []
 faces = []
-for index in range(0, 5):
-    bot.swipe(index)
 
+# przesunie 3 osob w ktoras strone
+for index in range(0, 3):
+    bot.swipe(index)
+# odpisywanie ludziom (w funkcji char jest ustawiony powrot po trzech osobach)
 bot.chat()
